aae244cc931028453fdd709f6947f9fc424ba5e4
cbeclinfo-dev <cbecl.info@gmail.com>
2026-02-23 05:08:41 +0600

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Project Consultant in Bangladesh";
+export const SITE_TITLE = "Project Consultant";
 export const SITE_DESCRIPTION = "I am a professional project consultant in Bangladesh providing project profile preparation service.";
